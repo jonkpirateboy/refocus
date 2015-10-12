32e7208ca59c64d858bae246460aa1201d8d8c32
Johan Jonk Stenström <jonk@pirateboy.net>
2015-10-12 11:24:06 +0200

    jquery.refocus.js
    jquery.refocus.js

---
+++ jquery.refocus.js
@@ -0,0 +1,19 @@
+/**
+ * jQuery Refocus
+ * A lightweight jQuery plugin to focus and unfocus like a camera
+ *
+ * https://github.com/jonkpirateboy
+ */
+
+jQuery.fn.extend({
+    refocus: function () {
+    	container = '.' + $(this).attr('class');
+        imgSrc = $(container + ' .refocus-img img').attr('src');
+		containerHeight = $(container + ' .refocus-img img').height()*0.8;
+		$(container + ' .refocus-img, ' + container + ' .refocus-img-bg').css('background-image','url('+imgSrc+')');
+		$(container + ', ' + container + ' .refocus-img, ' + container + ' .refocus-img-bg').css('height', containerHeight);
+		$(container + ' .focus-out, ' + container + ' .focus-in').addClass('refocus');
+		$(container + ' .focus-out.refocus').removeClass('focus-out refocus').addClass('focus-in');
+		$(container + ' .focus-in.refocus').removeClass('focus-in refocus').addClass('focus-out');
+    }
+});
