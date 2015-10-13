3d0eea97ae6131ce96198faf261748694e3321e8
jonkpirateboy <jonk@pirateboy.net>
2015-10-13 20:57:16 +0200

    change class name

--- jquery.refocus.js
+++ jquery.refocus.js
@@ -15,18 +15,18 @@ jQuery.fn.extend({
 	$(this).css('height', containerHeight);
 	$(this).css('min-height', contentHeight);
 	$(this).find('.refocus-img, .refocus-img-bg').css('background-image','url('+imgSrc+')');
-	$(this).find('.focus-out, .focus-in').addClass('refocus');
-	$(this).find('.focus-out.refocus').removeClass('focus-out refocus').addClass('focus-in');
-	$(this).find('.focus-in.refocus').removeClass('focus-in refocus').addClass('focus-out');
+	$(this).find('.focus-out, .focus-in').addClass('refocus-this');
+	$(this).find('.focus-out.refocus-this').removeClass('focus-out refocus-this').addClass('focus-in');
+	$(this).find('.focus-in.refocus-this').removeClass('focus-in refocus-this').addClass('focus-out');
 	var timer;
 	thisContainer = $(this);
 	$(window).resize(function () {
-		timer = setTimeout(function () { 
-			containerHeight = thisContainer.find('.refocus-img img').height()*0.8;
+	    timer = setTimeout(function () { 
+	        containerHeight = thisContainer.find('.refocus-img img').height()*0.8;
 			contentHeight = thisContainer.find('.refocus-text-container .refocus-text').height();
 			thisContainer.css('height', containerHeight);
 			thisContainer.css('min-height', contentHeight);
-		}, 500);
+	    }, 500);
 	});
     }
 });
