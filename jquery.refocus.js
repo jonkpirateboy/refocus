2d2ffd9d0d60019bcdfe03a0be4c32cd38b8e69a
Johan Jonk Stenström <jonk@pirateboy.net>
2015-10-13 13:15:42 +0200

    Update jquery.refocus.js

--- jquery.refocus.js
+++ jquery.refocus.js
@@ -9,25 +9,24 @@
 
 jQuery.fn.extend({
     refocus: function () {
-    	imgSrc = $(this).find('.refocus-img img').attr('src');
-		containerHeight = $(this).find('.refocus-img img').height()*0.8;
-		contentHeight = $(this).find('.refocus-text-container .refocus-text').height();
-		$(this).css('height', containerHeight);
-		$(this).css('min-height', contentHeight);
-		$(this).find('.refocus-img, .refocus-img-bg').css('background-image','url('+imgSrc+')');
-		$(this).find('.focus-out, .focus-in').addClass('refocus');
-		$(this).find('.focus-out.refocus').removeClass('focus-out refocus').addClass('focus-in');
-		$(this).find('.focus-in.refocus').removeClass('focus-in refocus').addClass('focus-out');
-		var timer;
-		thisContainer = $(this);
-		$(window).resize(function () {
-		    timer = setTimeout(function () { 
-		        containerHeight = thisContainer.find('.refocus-img img').height()*0.8;
-				contentHeight = thisContainer.find('.refocus-text-container .refocus-text').height();
-				thisContainer.css('height', containerHeight);
-				thisContainer.css('min-height', contentHeight);
-		    }, 500);
-		});
-		
+	imgSrc = $(this).find('.refocus-img img').attr('src');
+	containerHeight = $(this).find('.refocus-img img').height()*0.8;
+	contentHeight = $(this).find('.refocus-text-container .refocus-text').height();
+	$(this).css('height', containerHeight);
+	$(this).css('min-height', contentHeight);
+	$(this).find('.refocus-img, .refocus-img-bg').css('background-image','url('+imgSrc+')');
+	$(this).find('.focus-out, .focus-in').addClass('refocus');
+	$(this).find('.focus-out.refocus').removeClass('focus-out refocus').addClass('focus-in');
+	$(this).find('.focus-in.refocus').removeClass('focus-in refocus').addClass('focus-out');
+	var timer;
+	thisContainer = $(this);
+	$(window).resize(function () {
+	    timer = setTimeout(function () { 
+	        containerHeight = thisContainer.find('.refocus-img img').height()*0.8;
+			contentHeight = thisContainer.find('.refocus-text-container .refocus-text').height();
+			thisContainer.css('height', containerHeight);
+			thisContainer.css('min-height', contentHeight);
+	    }, 500);
+	});
     }
 });
